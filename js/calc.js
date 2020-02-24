45aa530baf95a0e27d93844e0b5f08ff730e65d2
sguerber <g.sarah.jobs@outlook.fr>
2020-02-24 16:21:22 +0100

    Débug de l'utilisation des floats et améliorations

--- js/calc.js
+++ js/calc.js
@@ -1,10 +1,13 @@
 /* Fait par Sarah Guerber le 12/01/2020 */
 
+// Initialisation des variables utilisées
 var chaine='';
 var stock1='';
 var stock2='';
 var op;
 var cptOp=0;
+var cptVirguleChiffre1=0;
+var cptVirguleChiffre2=0;
 
 /* fonction pour entrer des caractères */
 function ajoutTexte(b){
@@ -12,12 +15,29 @@ function ajoutTexte(b){
         affiche("Saisir un chiffre");
     }
     else if(cptOp==0 && isNaN(b) && b!='.'){  /* Si on a pas encore entré d'opérateur et que le char n'est ni un nombre ni un point (donc si c'est un opérateur) */
-        cptOp=cptOp+1;
+        cptOp=1;
+        if (chaine.slice(-1)=='.')
+        {
+            chaine=chaine.substring(0, chaine.length-1);
+        }
         chaine+=b;
         affiche(chaine);
     }
-    else if(cptOp>0 && isNaN(b) && b!='.'){  /* Si on  a déjà entré notre opérateur et qu'on tente d'en entrer un second */
-        affiche("Maximum une opération");
+    else if(cptOp==1 && isNaN(b) && b!='.'){  /* Si on a déjà entré notre opérateur et qu'on tente d'en entrer un second */
+        affiche("Max une opération");
+    }
+    else if ( b=='.' &&((cptOp==0 && cptVirguleChiffre1==1) || (cptOp==1 && cptVirguleChiffre2==1))){ /* Si on veut mettre une deuxième virgule dans une des deux opérandes */
+        affiche("Max une virgule /terme");
+    }
+    else if ( b=='.' && cptOp==0 && cptVirguleChiffre1==0){ /* Si on veut ajouter une virgule à la première opérande */
+        cptVirguleChiffre1=1;
+        chaine+=b;
+        affiche(chaine);
+    }
+    else if ( b=='.' && cptOp==1 && cptVirguleChiffre2==0){ /* Si on veut ajouter une virgule à la deuxième opérande */
+        cptVirguleChiffre2=1;
+        chaine+=b;
+        affiche(chaine);
     }
     else{   /* Dans les autres cas on continue d'entrer nos caractères dans la chaine et d'afficher celle-ci */
         chaine+=b;
@@ -59,7 +79,10 @@ function affiche(b){
 function CE(){
     chaine='';    
     document.getElementById("ecran").innerHTML=chaine;
+    /* On remet les compteurs des opérateurs et des virgules à zéro */
     cptOp=0;
+    cptVirguleChiffre1=0;
+    cptVirguleChiffre2=0;
 }
 
 /* fonction supprimant le deuxième nombre de l'opération */
@@ -68,32 +91,53 @@ function C(){
     var i=0;
     newchaine='';
     for (let char of chaine){
-        if(!isNaN(char)&&pasTrouve){  /* si le char est un chiffre et qu'on a pas encore trouvé l'opérateur */
+        if ((!isNaN(char)|| char=='.') && pasTrouve){  /* si le char est un chiffre et qu'on a pas encore trouvé l'opérateur */
             newchaine+=char;
         }
-        else{
-            pasTrouve=false; /* si l'opérateur a été trouvé...*/
-            if(isNaN(char)){  /* ...et que le char n'est pas un chiffre (donc c'est l'opérateur en question) */
+        else{ /* si l'opérateur a été trouvé...*/
+            pasTrouve=false; 
+            if(isNaN(char) && char!='.'){  /* ...et que le char n'est pas un chiffre (donc c'est l'opérateur en question) */
                 newchaine+=char;
             }      
         }
     }
+    cptVirguleChiffre2=0; /* on en profite pour remettre le compteur de virgules de la deuxième opérande à zéro */
     chaine=newchaine;
-    document.getElementById("ecran").innerHTML=newchaine;
+    affiche(chaine);
 }
 
 
 /* la fonction pour supprimer le dernier caractère entré */
 function supp(){
     if (chaine!=''){
-    chaine = chaine.replace(chaine.slice(-1), "");
-    affiche(chaine);
+        // si le dernier char est un point et qu'on a pas encore choisi d'opérateur (première opérande)
+        if(chaine.slice(-1)=='.' && cptOp==0)
+        {
+            cptVirguleChiffre1=0;
+        }
+        // si le dernier char est un point et qu'on a déjà choisi l'opérateur (seconde opérande)
+        else if (chaine.slice(-1)=='.' && cptOp==1)
+        {
+            cptVirguleChiffre2=0;
+        }
+        // si le dernier char est l'opérateur
+        else if(isNaN(chaine.slice(-1)) && chaine.slice(-1)!='.')
+        {
+            cptOp=0;
+            op=null;
+        }
+        // dans tous les cas on supprime le dernier élément de la chaine
+        chaine=chaine.substring(0,chaine.length-1);
+        affiche(chaine);
     }
 }
 
 /* la fonction pour afficher le résultat quand on clique sur la touche = */
 function resultat(){
+    // on initialise les variables à 0 car on commence avec le premier caractère de la chaine
     cptOp=0;
+    cptVirguleChiffre1=0;
+    cptVirguleChiffre2=0;
     for (let char of chaine){ /* pour tous les char de la chaine */
         if((!isNaN(char)||char=='.') && cptOp==0){ /* si le char est un chiffre ou une virgule et qu'on a pas encore cliqué sur un opérateur */
             stock1+=char;      /* on stocke le premier nombre, chiffre après chiffre */
@@ -106,13 +150,11 @@ function resultat(){
                 cptOp+=1;
                 op=char;
             }
-            else{   /* si l'utilisateur tente d'entrer une seconde opération on affiche un message */
-                affiche("Maximum une opération");  
-            }
+            
         }
         
     }
-    /* affichage des différentes opérations */
+    /* affichage du résultat en fonction de l'opérateur choisi */
     switch(op){
         case "+":
             chaine=add(stock1,stock2);
@@ -138,4 +180,6 @@ function resultat(){
     stock2='';
     chaine='';
     cptOp=0;
+    cptVirguleChiffre1==0;
+    cptVirguleChiffre2==0;
 }
